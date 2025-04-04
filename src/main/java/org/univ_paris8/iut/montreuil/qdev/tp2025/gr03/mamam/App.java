95d1383f8a0593c73cb0444f82172e54e2f9340d
maeveolo <maeveaoloughlin@gmail.com>
2025-04-04 14:16:35 +0200

    init

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr03/mamam/App.java
@@ -0,0 +1,10 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr03.mamam;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
